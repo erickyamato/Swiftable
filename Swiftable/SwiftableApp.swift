029971f5f15560dab025e66c5d27d9b51f5c14a2
Erick Yamato <erick.yamato@me.com>
2022-10-25 22:54:10 -0300

    Initial Commit

---
+++ Swiftable/SwiftableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftableApp.swift
+//  Swiftable
+//
+//  Created by Erick Yamat on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
