d0f5dacb54759fc0cec0fc3916ad829fa9634db8
Cecilia Andrea Pesce <magicalkind@gmail.com>
2022-11-13 19:07:38 -0300

    add AreaMark with two data sets

--- Swiftable/ContentView.swift
+++ Swiftable/ContentView.swift
@@ -12,6 +12,7 @@ struct ChartData: Identifiable {
     let id = UUID()
     let value: Double
     let month: Date
+    var type: String
 }
 
 
@@ -19,19 +20,41 @@ struct ContentView: View {
     @State private var range: (Date, Date)? = nil
     
     let data: [ChartData] = [
-        ChartData(value: 300, month: date(year: 2022, month: 9, day: 2)),
-        ChartData(value: 100, month: date(year: 2022, month: 10, day: 2)),
-        ChartData(value: 250, month: date(year: 2022, month: 11, day: 2)),
-        ChartData(value: 80, month: date(year: 2022, month: 12, day: 2))
+        ChartData(value: 5,
+                  month: date(year: 2022, month: 9, day: 2),
+                  type: "Erick Steps"),
+        ChartData(value: 4,
+                  month: date(year: 2022, month: 10, day: 2),
+                  type: "Erick Steps"),
+        ChartData(value: 7,
+                  month: date(year: 2022, month: 11, day: 2),
+                  type: "Erick Steps"),
+        ChartData(value: 15,
+                  month: date(year: 2022, month: 12, day: 2),
+                  type: "Erick Steps"),
+        ChartData(value: 15,
+                  month: date(year: 2022, month: 9, day: 2),
+                  type: "Ceci Steps"),
+        ChartData(value: 14,
+                  month: date(year: 2022, month: 10, day: 2),
+                  type: "Ceci Steps"),
+        ChartData(value: 17,
+                  month: date(year: 2022, month: 11, day: 2),
+                  type: "Ceci Steps"),
+        ChartData(value: 25,
+                  month: date(year: 2022, month: 12, day: 2),
+                  type: "Ceci Steps")
+        
     ]
     
     var body: some View {
         GroupBox(String()) {
-            Text("BarMark")
+            Text("AreaMark")
             Chart {
                 ForEach(data, id: \.month) {
-                    BarMark(x: .value("Date", $0.month, unit: .month),
+                    AreaMark(x: .value("Date", $0.month, unit: .month),
                             y: .value("Steps", $0.value))
+                    .foregroundStyle(by: .value("Type", "\($0.type)"))
                 }
             }
             .chartYAxis {
