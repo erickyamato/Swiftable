52dc946458d741fb151086543935bb8e577a7ae3
Erick Yamato <erick.yamato@me.com>
2022-11-13 18:29:53 -0300

    first commit

---
+++ Swiftable/GeometryReader/BarView.swift
@@ -0,0 +1,20 @@
+//
+//  BarView.swift
+//  Swiftable
+//
+//  Created by Erick Yamat on 30/10/22.
+//
+
+import SwiftUI
+
+struct BarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BarView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarView()
+    }
+}
